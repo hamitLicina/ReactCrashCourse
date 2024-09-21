23423bb7218e1f2fc5d04c06440e56091a058f21
hamitLicina <hamitlicina@gmail.com>
2024-09-21 19:17:28 +0300

    Pages folder and Layout folder added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@
 import './App.css'
 
 function App() {
-  
+
   return (
     <>
       <div className='text-5xl'>
-        <h1>React Crash Course</h1> 
+        <h1>React Crash Course</h1>
       </div>
     </>
   )
