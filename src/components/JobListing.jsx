84b7cfc5ccc1fe622e464d570000bf0c094cf16a
hamitLicina <hamitlicina@gmail.com>
2024-11-02 04:40:53 +0300

    Update configuration files and JobListing component; add ESLint config file

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import { FaMapMarker } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
-
-
 const JobListing = ({ job }) => {
     const [showFullDescription, setShowFullDescription] = useState(false);
 
@@ -23,7 +21,10 @@ const JobListing = ({ job }) => {
 
                 <div className='mb-5'>{description}</div>
 
-                <button onClick={() => setShowFullDescription((prevState) => !prevState)} className='text-indigo-500 mb-5 hover:text-indigo-600' >
+                <button
+                    onClick={() => setShowFullDescription((prevState) => !prevState)}
+                    className='text-indigo-500 mb-5 hover:text-indigo-600'
+                >
                     {showFullDescription ? 'Less' : 'More'}
                 </button>
 
@@ -36,7 +37,10 @@ const JobListing = ({ job }) => {
                         <FaMapMarker className='inline text-lg mb-1 mr-1' />
                         {job.location}
                     </div>
-                    <Link to={`/jobs/${job.id}`} className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm' >
+                    <Link
+                        to={`/jobs/${job.id}`}
+                        className='h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm'
+                    >
                         Read More
                     </Link>
                 </div>
